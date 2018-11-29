587a6b06cbb847c49f1561f968e2bd4a32fcbe8c
Ting Wang <felix.wang.1026@gmail.com>
2018-11-28 22:54:30 -0500

    added more scenarios of linked list

--- basics-data-structure/linkedList.js
+++ basics-data-structure/linkedList.js
@@ -5,3 +5,41 @@ class ListNode {
         this.next = null;
     }
 }
+
+// reverse linked list
+class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    reverse(head) {
+        let prev = null;
+        while (head) {
+            let temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
+
+class DLinkNode {
+    constructor(value) {
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+
+    reverse(head) {
+        let current = null;
+        while (head) {
+            current = head;
+            head = current.next;
+            current.next = current.prev;
+            current.prev = head;
+        }
+        return current;
+    }
+}
--- basics-data-structure/linkedList.py
+++ basics-data-structure/linkedList.py
@@ -3,3 +3,37 @@ class LinkNode:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+
+#  reverse linked list
+class LinkNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def reverse(self, head):
+        prev = None
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        return prev
+
+
+class DLinkNode:
+    def __init__(self, value):
+        self.value = value
+        self.prev = self.next = None
+
+    def reverse(self, head):
+        current = None
+        while head:
+            current = head
+            head = current.next
+            current.next = current.prev
+            current.prev = head
+        return current
+
+
+
