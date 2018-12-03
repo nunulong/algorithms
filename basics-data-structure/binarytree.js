19f669b58957cd0e8552de028eb1648fe77a8706
Ting Wang <felix.wang.1026@gmail.com>
2018-12-02 23:24:34 -0500

    added three different traversal methods for BST

--- basics-data-structure/binarytree.js
+++ basics-data-structure/binarytree.js
@@ -5,3 +5,33 @@ class TreeNode {
         this.right = null;
     }
 }
+
+class Traverse {
+    constructor() {
+        this.traverse = [];
+    }
+
+    preorder(root) {
+        if (root) {
+            this.traverse.push(root.value);
+            this.preorder(root.left);
+            this.preorder(root.right);
+        }
+    }
+
+    inorder(root) {
+        if (root) {
+            this.inorder(root.left);
+            this.traverse.push(root.value);
+            this.inorder(root.right);
+        }
+    }
+
+    postorder(root) {
+        if (root) {
+            this.postorder(root.left);
+            this.postorder(root.right);
+            this.traverse.push(root.value);
+        }
+    }
+}
--- basics-data-structure/binarytree.py
+++ basics-data-structure/binarytree.py
@@ -2,3 +2,26 @@ class TreeNode:
     def __init__(self, value):
         self.value = value
         self.left, self.right = None, None
+
+
+class Traverse(object):
+    def __init__(self):
+        self.traverse_path = list()
+
+    def preorder(self, root):
+        if root:
+            self.traverse_path.append(root.value)
+            self.preorder(root.left)
+            self.preorder(root.right)
+
+    def inorder(self, root):
+        if root:
+            self.inorder(self.left)
+            self.traverse_path.append(root.value)
+            self.inorder(self.right)
+
+    def postorder(self, root):
+        if root:
+            self.postorder(self, root.left)
+            self.postorder(self, root.right)
+            self.traverse_path.append(root.value)
