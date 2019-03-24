3c922c352294dd36d1aa7391352e2a943fe5c77a
Ting Wang <felix.wang.1026@gmail.com>
2019-03-24 00:24:51 -0400

    refactored sumLinked

--- coding-challenges/sumLinked.js
+++ coding-challenges/sumLinked.js
@@ -5,33 +5,29 @@ class Node {
     }
 }
 
+const linkedToSum = (linked) => {
+    if (!linked) return null;
+    let num = '';
+    let cur = linked;
+    do {
+        num += cur.val.toString();
+        cur = cur.next;
+    } while (cur);
+    return Number(num.split('').reverse().join(''));
+}
+
 const sumLinked = (l1, l2) => {
     if (!l1) return l2;
     if (!l2) return l1;
-    if (!l1 && !l2) return null;
-    let num1 = '';
-    let cur1 = l1;
-    do {
-        num1 += cur1.val.toString();
-        cur1 = cur1.next;
-    } while (cur1);
-    num1 = Number(num1.split('').reverse().join(''));
-    console.log(num1);
-    let num2 = '';
-    let cur2 = l2;
-    do {
-        num2 += cur2.val.toString();
-        cur2 = cur2.next;
-    } while (cur2);
-    num2 = Number(num2.split('').reverse().join(''));
-    console.log(num2);
+
+    let num1 = linkedToSum(l1);
+    let num2 = linkedToSum(l2);
     let num = num1 + num2;
-    console.log(num);
-    const result = new Node(num % 10);
+    num = num.toString().split('').reverse();
+    const result = new Node(Number(num[0]));
     let cur = result;
-    while ((num - num % 10) / 10 > 0) {
-        num = (num - num % 10) / 10
-        cur.next = new Node(num % 10);
+    for (let i = 1; i < num.length; i++) {
+        cur.next = new Node(Number(num[i]));
         cur = cur.next;
     }
     return result;
