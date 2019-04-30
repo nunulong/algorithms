1a7bbedd1658b3a07f562b15265fb764d8285c19
Ting Wang <felix.wang.1026@gmail.com>
2019-04-30 00:45:53 -0400

    optimal solution

--- coding-challenges/onewayflight.js
+++ coding-challenges/onewayflight.js
@@ -1,23 +1,40 @@
+// const onewayFlight = (tickets) => {
+//     const flight = [];
+//     let start;
+//     const cloneTickets = tickets.slice(0);
+//     cloneTickets.forEach((ticket, index) => {
+//         if (ticket[0] == null) {
+//             start = ticket[1];
+//             cloneTickets.splice(index, 1);
+//         }
+//     });
+//     flight.push(start);
+//     while (flight.length !== tickets.length - 1) {
+//         cloneTickets.forEach((ticket, index) => {
+//             if (start === ticket[0] && ticket[1]) {
+//                 start = ticket[1];
+//                 flight.push(start);
+//                 cloneTickets.splice(index, 1);
+//             }
+//         });
+//     }
+//     return flight;
+// }
+
 const onewayFlight = (tickets) => {
-    const flight = [];
-    let start;
-    const cloneTickets = tickets.slice(0);
-    cloneTickets.forEach((ticket, index) => {
+    const hash = {};
+    const flight = Array(tickets.length - 1);
+    tickets.forEach(ticket => {
         if (ticket[0] == null) {
-            start = ticket[1];
-            cloneTickets.splice(index, 1);
+            flight[0] = ticket[1];
         }
+        hash[ticket[0]] = ticket[1];
     });
-    flight.push(start);
-    while (flight.length !== tickets.length - 1) {
-        cloneTickets.forEach((ticket, index) => {
-            if (start === ticket[0] && ticket[1]) {
-                start = ticket[1];
-                flight.push(start);
-                cloneTickets.splice(index, 1);
-            }
-        });
+
+    for (let i = 1; i < flight.length; i++) {
+        flight[i] = hash[flight[i - 1]];
     }
+
     return flight;
 }
 
